13a8140e8c4753708576ec6fd22417a554baa503
brbredbharat <bdassher@gmail.com>
2025-06-29 15:38:54 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,20 +1,15 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from telegram import Bot
-from news_fetcher import get_top_3_stocks
-import os
+from apscheduler.triggers.cron import CronTrigger
+import pytz
 
-def send_daily_stock_news():
-    TOKEN = os.environ.get("TOKEN")
-    CHAT_ID = os.environ.get("CHAT_ID")
-    if not TOKEN or not CHAT_ID:
-        print("TOKEN or CHAT_ID not set")
-        return
+scheduler = BackgroundScheduler()
 
-    bot = Bot(token=TOKEN)
-    message = get_top_3_stocks()
-    bot.send_message(chat_id=CHAT_ID, text=message, parse_mode="Markdown")
+# correct way
+india_timezone = pytz.timezone('Asia/Kolkata')
 
-def schedule_daily_job():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(send_daily_stock_news, trigger='cron', hour=9, minute=0)
-    scheduler.start()
+scheduler.add_job(
+    send_daily_stock_news, 
+    CronTrigger(hour=9, minute=0, timezone=india_timezone)
+)
+
+scheduler.start()
